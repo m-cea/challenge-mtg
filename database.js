f2ddcfa69713b84e098fed4333c9d0ce4bf53bd7
m-cea <mc1317@hotmail.com>
2023-05-11 01:36:18 -0300

    conexion mongoatlas

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/tablaPosiciones'
+const MONGODB_URI = 'mongodb+srv://liccealaboral:challenge2023@cluster0.xled8cl.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URI);
 mongoose.connection.once('open', _ => {
     console.log('Database connected to ' + MONGODB_URI);
