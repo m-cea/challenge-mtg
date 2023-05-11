3febba5208beb939640a37ac7c163342ff0ea779
m-cea <mc1317@hotmail.com>
2023-05-11 02:57:11 -0300

    eliminados usos de prueba

--- dist/routes/rutas.js
+++ dist/routes/rutas.js
@@ -2,20 +2,12 @@ const express = require('express');
 const router = express.Router()
 require('../../database');
 const Equipo = require('../../Models/Equipos');
-const Brasil = require('../../Models/Brasil');
-const br = require('../scrapBrasil.js')
 const scrap = require('../scrap.js')
 
 router.get("/", async (req, res) => {
     scrap()
     const equipos = await Equipo.find().sort('posicion').lean()
     res.render("index.hbs", {equipos: equipos})
-});
-
-router.get("/brasil", async (req, res) => {
-    br()
-    const brasil = await Brasil.find().sort('posicion').lean()
-    res.render("brasil.hbs", {brasil: brasil})
 });
 
 module.exports = router;
